97e809076c5ae0e312708cc5bb24df823d580ce4
unknown <james.l.akerman@gmail.com>
2021-12-13 00:16:41 +1100

    final draft

--- static/logic.js
+++ static/logic.js
@@ -89,28 +89,6 @@ var pie_Google_chart_data = [["Stocks", "Current Values"]]
 for (let i = 0; i < stock_totals_dict_values.length; i++){
     pie_Google_chart_data.push([stock_totals_dict_keys[i],stock_totals_dict_values[i]])
 }
-// Create the Google Pie Chart
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-var data = google.visualization.arrayToDataTable(pie_Google_chart_data);
-var options = {
-    titleTextStyle: {
-        color: "black", 
-        fontName: "Arial",
-        fontSize: 20,
-        bold: false, 
-        italic: false
-    },
-title:"All Client Portfolio Stocks in $AU and % of Total Current Value Managed",
-  height: 800,
-  width: 800
-}
-var chart = new google.visualization.PieChart(document.getElementById('myChart'));
-  chart.draw(data, options);
-};
-
-
 
 // Show the individual stock values for the porfolio of each client
 var current_portfolios_values_dict = {};
@@ -137,6 +115,8 @@ var hor_bar_chart = new Chart("bar_chart", {
   }],
 },
 options: {
+    responsive: true,
+    maintainAspectRatio: false,
     indexAxis: 'y',
     scales: {
         x: {
@@ -150,6 +130,7 @@ options: {
                     family: 'Arial',
                     weight: 1,
                     },
+                    color: "black",
             }
         },
         y: {
@@ -159,6 +140,7 @@ options: {
                     family: 'Arial',
                     weight: 1,
                     },
+                color: "black"
             }
         }
     },
@@ -177,19 +159,30 @@ options: {
             }
         }
     });
-    // Create a pie chart
-    var piedata = [{
-            type: 'pie',
-            labels: stock_totals_dict_keys,
-            values: stock_totals_dict_values
-          }];
-    var pie_layout = {
-        title: "All Client Portfolio Stocks in $AU and" + "<br>" 
-        + "% of Total Current Value Managed",
-        height: 550,
-        width: 550
-        };
-    Plotly.newPlot('pie_chart', piedata, pie_layout);
+    // Create a pie chart with Google Charts
+    // Create the Google Pie Chart
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+var data = google.visualization.arrayToDataTable(pie_Google_chart_data);
+var options = {
+    titleTextStyle: {
+        color: "black", 
+        fontName: "Arial",
+        fontSize: 20,
+        bold: false, 
+        italic: false
+    },
+'title':"All Client Portfolio Stocks in $AU and % of Total Current Value Managed",
+  'height': 500,
+  'width': 500,
+  'chartArea': {'width': '100%', 'height': '80%'},
+               'legend': {'position': 'bottom'}
+}
+var chart = new google.visualization.PieChart(document.getElementById('pie_chart'));
+  chart.draw(data, options);
+};
+    
 
     // Create Line chart
     createLineChart("default")
@@ -206,8 +199,6 @@ options: {
             // filter the values
             bar_keys = ["Total Purchase Value", "Total Current Value"]
             bar_values = [portfolio_totals_dict[value], portfolio_purchase_totals_dict[value]]
-            pie_keys = current_portfolios_values_dict[value][0]
-            pie_values = current_portfolios_values_dict[value][1]
             line_stocks = current_portfolios_values_dict[value][0]
             colors = ['orange', 'green',]
             // Filter the pie data
@@ -218,21 +209,19 @@ options: {
             pie_data = filtered_pie_Google_chart_data
             console.log(filtered_pie_Google_chart_data)
 
-            createCharts(bar_keys, bar_values, pie_keys, pie_values, line_stocks, value, colors, hor_bar_chart, pie_data)
+            createCharts(bar_keys, bar_values, line_stocks, value, colors, hor_bar_chart, pie_data)
         }
         else{
             // reset the values
             bar_keys = filtered_portfolio_keys
             bar_values = filtered_portfolio_values
-            pie_keys = stock_totals_dict_keys
-            pie_values = stock_totals_dict_values
             line_stocks = "default"
             colors = ['blue']
             pie_data = pie_Google_chart_data
-            createCharts(bar_keys, bar_values, pie_keys, pie_values, line_stocks, value, colors, hor_bar_chart, pie_data)
+            createCharts(bar_keys, bar_values, line_stocks, value, colors, hor_bar_chart, pie_data)
         }
     }
-    function createCharts (bar_k, bar_v, pie_k, pie_v, line_v, client_name, colors, oldbar, pie_data){
+    function createCharts (bar_k, bar_v, line_v, client_name, colors, oldbar, pie_data){
         // Remove the old bar chart
         oldbar.destroy();
         // Create a horizontal bar chart
@@ -246,6 +235,8 @@ options: {
             }],
           },
           options: {
+            responsive: true,
+            maintainAspectRatio: false,
             indexAxis: 'y',
             scales: {
                 x: {
@@ -259,6 +250,7 @@ options: {
                             family: 'Arial',
                             weight: 1,
                             },
+                            color: "black",
                     }
                 },
                 y: {
@@ -268,6 +260,7 @@ options: {
                             family: 'Arial',
                             weight: 1,
                             },
+                            color: "black",
                     }
                 }
             },
@@ -288,19 +281,6 @@ options: {
           });
 
         // Create a pie chart
-        var piedata = [{
-            type: 'pie',
-            values: pie_v,
-            labels: pie_k,
-        }];
-        var pie_layout = {
-            title: "Stocks owned by " + client_name + "in $AU" + "<br>" + "and % of Total Current Portfolio Value",
-            height: 550,
-            width: 550
-            };
-        Plotly.newPlot('pie_chart', piedata, pie_layout);
-
-    // Create the Google Pie Chart
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
@@ -313,11 +293,13 @@ var options = {
             bold: false, 
             italic: false
         },
-  title:"Stocks owned by " + client_name + "in $AU " + "and % of Total Current Portfolio Value"
-  ,height: 800
-  ,width: 800
+  'title':"Stocks owned by " + client_name + "in $AU " + "and % of Total Current Portfolio Value",
+  'height': 500,
+  'width': 500,
+  'chartArea': {'width': '100%', 'height': '80%'},
+               'legend': {'position': 'bottom'}
 };
-var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+var chart = new google.visualization.PieChart(document.getElementById('pie_chart'));
   chart.draw(data, options);
 };
 
@@ -391,6 +373,7 @@ var chart = new google.visualization.PieChart(document.getElementById('myChart')
                     chart_data.push(trace)
                 }
                 var line_layout = {
+                   
                     title: {
                         text: chart_title,
                         font: {
@@ -405,10 +388,10 @@ var chart = new google.visualization.PieChart(document.getElementById('myChart')
                             font: {
                               color: 'black',
                               family: 'Arial',
-                              size: 16,
+                              size: 14,
                         }
                     },
-                    height: 900,
+                    height: 700,
                     width: 1400,
                     xaxis: {
                         title: {
